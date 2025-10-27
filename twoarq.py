c3c588d54a6a16bb010bfc60aaa99fb8ab4267a6
JULIO CESAR BATISTA DE LIMA <202502458371@alunos.unifbv.edu.br>
2025-10-27 09:27:52 -0300

    ok

--- twoarq.py
+++ twoarq.py
@@ -1,6 +1,13 @@
-a = int(input("Digite o primeiro numero:  "))
-b = int(input("Digite o segundo numero"))
+try:
+    a = int(input("Digite o primeiro numero:  "))
+    b = int(input("Digite o segundo numero"))
 
-resultado = a + b
-print(f"Resultado: {resultado}")
-print("Progama encerrado mcom sucesso!")
+    resultado = a / b
+    print(f"Resultado: {resultado}")
+except ZeroDivisionError:
+    print("Erro: Divisao por zero nao e permitida.")
+except ValueError:
+    print("Erro: Valor invalido.")
+finally:
+    print("Encerrando execução...")
+
