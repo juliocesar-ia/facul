648b5a007fdd0705d1d1a47155ba18e33c14c4a1
JULIO CESAR BATISTA DE LIMA <202502458371@alunos.unifbv.edu.br>
2025-10-27 09:10:11 -0300

    nov

---
+++ twoarq.py
@@ -0,0 +1,6 @@
+a = int(input("Digite o primeiro numero:  "))
+b = int(input("Digite o segundo numero"))
+
+resultado = a + b
+print(f"Resultado: {resultado}")
+print("Progama encerrado mcom sucesso!")
